88756b47bd9360579ea3ddfd2863d882d365aa70
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-10-02 14:10:33 +0700

    test_pot

--- f103c8_esc.ino
+++ f103c8_esc.ino
@@ -1,71 +1,10 @@
-int count = 1;
-bool cw = true;
+int potVal;
 
-unsigned long prevMicros = 0;
-unsigned long prevMillis = 0;
-
-void setup() {
-  pinMode(PA0, OUTPUT);
-  pinMode(PA1, OUTPUT);
-  pinMode(PA3, OUTPUT);
-
-  analogWrite(PA0, LOW);
-  analogWrite(PA1, LOW);
-  analogWrite(PA3, LOW);
-  
+void setup(){
+  pinMode(A0, OUTPUT);
 }
-int i = 0;
-float pwm_in = 1000;
-void loop() {
-  unsigned long currentMicros = micros();
-  unsigned long currMillis = millis();
 
-  if(currMillis - prevMillis >= 1000){
-    prevMillis = currMillis;
-    
-    if(cw){
-      pwm_in = pwm_in - 100;
-  
-      if(pwm_in == 0){
-        cw = false;
-      }
-    }else{
-      pwm_in = pwm_in + 100;
-      if(pwm_in == 1000){
-        cw = true;
-      }
-    }
-
-    
-  }
-  
-  
-  if(currentMicros - prevMicros >= pwm_in){
-    
-    prevMicros = currentMicros;
-    i++;
-
-    if(count == 1){
-      analogWrite(PA0, i);
-      analogWrite(PA1, 0);
-      analogWrite(PA3, 0);
-    }else if(count == 2){
-      analogWrite(PA0, 0);
-      analogWrite(PA1, i);
-      analogWrite(PA3, 0);
-    }else if(count == 3){
-      analogWrite(PA0, 0);
-      analogWrite(PA1, 0);
-      analogWrite(PA3, i);
-    }else{
-      count = 1;
-    }
-    
-    if(i == 255){
-      i = 0;
-      count++;
-    }
-  }
-  
-    
+void loop(){
+  potVal = map(analogRead(A7), 0, 1023, 0, 255);
+  analogWrite(A0, potVal);
 }
